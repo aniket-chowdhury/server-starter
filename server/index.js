18af4bfe74957d9616d90ce229cf336eb104dcd7
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2019-05-13 16:21:57 +0530

    Authentication middleware

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ const server = require('./server')
 const port = process.env.PORT;
 const staticDir = __dirname + "/./static/";
 const routesDir = __dirname + "/./routes/";
+const middlewaresDir = __dirname + "/./middlewares/";
 
 // Express Initialization
 const app = express();
@@ -20,6 +21,11 @@ app.use(express.json());
 
 // Login Routes
 app.use('/login',require(routesDir + "loginRoutes"))
+
+//  Testing routes -> to be remove in Production
+app.use('/tests',express.static('tests'))
+app.use('/',require(middlewaresDir+'auth'))
+app.get('/',(req,res)=>{res.status(200).send('okay')})
 
 console.log("\n\b");
 

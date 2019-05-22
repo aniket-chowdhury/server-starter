75144a26c5b2bd10a76d1797f5dcc1671f8e3a90
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2019-05-22 17:05:35 +0530

    mongo automation for loginRoutes

--- server/routes/loginRoutes.js
+++ server/routes/loginRoutes.js
@@ -7,50 +7,109 @@ const assert = require('assert');
 
 const app = express.Router();
 
+const getJWT = options => {};
+
 const login = options => {
 	return (req, res, next) => {
-		const username = options['username'];
-		const password = options['password'];
-		if (username && password) {
-			if (req.body.username == username) {
-				if (
-					bcrypt.compareSync(req.body.password, process.env._PASSWORD)
-				) {
-					const data = options['data'] || username;
-					const expiry = options['expiry'] || 24 * 60 * 60;
-					const key = options['key'];
+		try {
+			if (
+				(options['mongo'] && options['password']) ||
+				(options['mongo'] && options['key'])
+			) {
+				throw `Cannot have password and/or key as an option with mongo:true`;
+			}
+
+			const configure = options => {
+				const model = options['mongo'];
+				if (model) {
+					const username = docs['username'];
 					try {
-						assert(key);
-						jwt.sign(
-							{ data: data },
-							key,
-							{ expiresIn: expiry },
-							(err, token) => {
+						mongo.findOne(
+							{ username: username },
+							'_id password',
+							(err, docs) => {
 								if (err) {
-									console.log(colors.error(`--- ${err}`));
-									res.status(500).send(
-										'Internal Server Error. Error generating token.'
-									);
-								} else {
-									res.status(200).json({ token: token });
+									throw err;
 								}
+								const password = docs['password'];
+								const key = docs['_id'];
+								return {
+									username: username,
+									password: password,
+									key: key
+								};
 							}
 						);
 					} catch (err) {
-						console.log(colors.error(err));
-						return res.status(500).send('Internal Server Error');
+						console.log(colors.err('Failed to respond.'));
+						res.status(502).send(
+							'Bad Gateway. Data Response Failure.'
+						);
 					}
+				} else {
+					return {
+						username: options['username'],
+						password: options['password'],
+						key: options['key']
+					};
+				}
+			};
+
+			const configuration = configure(options);
+			options['username'] = configuration['username'];
+			options['password'] = configuration['password'];
+			options['key'] = configuration['key'];
+
+			const username = configuration['username'];
+			const password = configuration['password'];
+
+
+			if (username && password) {
+				if (req.body.username == username) {
+					if (
+						bcrypt.compareSync(
+							req.body.password,
+							process.env._PASSWORD
+						)
+					) {
+						const data = options['data'] || username;
+						const expiry = options['expiry'] || 24 * 60 * 60;
+						const key = options['key'];
+						try {
+							assert(key);
+							jwt.sign(
+								{ data: data },
+								key,
+								{ expiresIn: expiry },
+								(err, token) => {
+									if (err) {
+										console.log(colors.error(`--- ${err}`));
+										res.status(500).send(
+											'Internal Server Error. Error generating token.'
+										);
+									} else {
+										res.status(200).json({ token: token });
+									}
+								}
+							);
+						} catch (err) {
+							console.log(colors.error(err));
+							return res
+								.status(500)
+								.send('Internal Server Error');
+						}
+					}
+				} else {
+					return res
+						.status(400)
+						.send('Username or password not found.');
 				}
 			} else {
 				return res.status(400).send('Username or password not found.');
 			}
-		} else {
-			return res.status(400).send('Username or password not found.');
-		}
+		} catch (err) {}
 	};
 };
-
-
 
 app.post(
 	'/',
@@ -59,8 +118,9 @@ app.post(
 		password: process.env._PASSWORD,
 		key: process.env._USERID,
 		expiry: 60 * 60 * 24,
-		mongo:false
+		mongo: false
 	})
 );
 
 module.exports = app;
+// export default app;

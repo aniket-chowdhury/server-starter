43358ff7c6b4331f74345faffebc754ca31a30bd
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2019-06-07 00:08:22 +0530

    add middleware -> not tested

--- server/routes/api/addItem.js
+++ server/routes/api/addItem.js
@@ -3,20 +3,10 @@ const colors = require(__dirname + '/../../colors');
 const express = require('express');
 const Item = require(__dirname + '/../../models/Item');
 
+const add = require('../../middlewares/crudMethods/add')
+
 const app = express.Router();
 
-app.post('/', (req, res, next) => {
-	const data = new Item(req.body);
-	data.save(err => {
-		if (err) {
-			console.log(colors.error(`--- ${err}`));
-			res.status(500).send(
-				"Internal Server Error. Can't save to database"
-			);
-		} else {
-			res.status(200).json({ success: true });
-		}
-	});
-});
+app.post('/', add({model:Item}));
 
 module.exports = app;

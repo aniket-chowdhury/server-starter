0e5f167b079781b76beab64fd6201a6067697f83
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2019-06-07 00:06:06 +0530

    delete middleware -> not tested

--- server/routes/api/deleteItem.js
+++ server/routes/api/deleteItem.js
@@ -3,25 +3,10 @@ const colors = require(__dirname + '/../../colors');
 const express = require('express');
 const Item = require(__dirname + '/../../models/Item');
 
+const deleteItem = require('../../middlewares/crudMethods/delete')
+
 const app = express.Router();
 
-app.post('/', (req, res, next) => {
-	const id = req.body.itemId || req.query.itemId;
-	if (id) {
-		Item.findByIdAndDelete(id, req.body, err => {
-			if (err) {
-				console.log(colors.error(`--- ${err}`));
-				res.status(502).send('Bad Gateway. Data Response Failure.');
-			} else {
-				res.status(200).json({
-					success: true,
-					message: 'Item deleted'
-				});
-			}
-		});
-	} else {
-		res.status(417).send('Expectation failed.');
-	}
-});
+app.post('/', deleteItem({model:Item}));
 
 module.exports = app;

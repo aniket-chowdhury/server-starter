11dcd339d75f7a143f45450ef07064ccde94e9b9
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2019-12-01 21:01:20 +0530

    App

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "bootstrap-css-only/css/bootstrap.min.css";
-import "mdbreact/dist/css/mdb.css";
-import './styles/index.scss';
-import Routes from './scripts/Routes';
+
+import App from './scripts/App';
+
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import './styles/index.scss';
 
 
-ReactDOM.render(<Routes/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));

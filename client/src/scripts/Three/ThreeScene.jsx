5cc574f66c7b239f4944cef61049ad55756cad42
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2019-12-01 20:51:05 +0530

    three-js-update on zoom

--- client/src/scripts/Three/ThreeScene.jsx
+++ client/src/scripts/Three/ThreeScene.jsx
@@ -1,90 +1,104 @@
 import React, { Component } from 'react'
-import * as THREE from 'three';
+import * as THREE from 'three'
 import OrbitControls from 'three-orbitcontrols'
 
 export default class ThreeScene extends Component {
-    constructor(props) {
-        super(props)
-        this.start = this.start.bind(this)
-        this.stop = this.stop.bind(this)
-        this.animate = this.animate.bind(this)
-        this.createCube = this.createCube.bind(this)
-    }
-
-    componentDidMount() {
-        const width = this.mount.clientWidth
-        const height = this.mount.clientHeight
-
-        const scene = new THREE.Scene()
-
-        const camera = new THREE.PerspectiveCamera(
-            75,
-            width / height,
-            1,
-            10000000
-        )
-        const renderer = new THREE.WebGLRenderer({ antialias: true })
-        renderer.setClearColor('#000000')
-        renderer.setSize(width, height)
-        this.scene = scene
-
-        scene.add(this.createCube())
-
-        camera.position.z = 3
-
-
-        this.camera = camera
-        this.renderer = renderer
-
-        new OrbitControls(camera,renderer.domElement)
+	constructor(props) {
+		super(props)
+		this.start = this.start.bind(this)
+		this.stop = this.stop.bind(this)
+		this.animate = this.animate.bind(this)
+		this.createCube = this.createCube.bind(this)
+	}
+	log() {
+        this.width = this.mount.clientWidth
+        this.height = this.mount.clientHeight
         
-        this.mount.appendChild(this.renderer.domElement)
-        this.start()
-    }
+		console.log('resize', this.width, this.height)
+        
+        this.renderer.setSize(this.width, this.height)
+        this.camera.aspect = this.width / this.height
+        this.camera.updateProjectionMatrix()
 
-    componentWillUnmount() {
-        this.stop()
-        this.mount.removeChild(this.renderer.domElement)
-    }
+	}
 
-    createCube() {
-        const geometry = new THREE.BoxGeometry(1, 1, 1)
-        const material = new THREE.MeshBasicMaterial({ color: '#00ff00' })
-        const cube = new THREE.Mesh(geometry, material)
+	componentDidMount() {
+		window.addEventListener('resize', this.log.bind(this))
+		let width = this.mount.clientWidth
+		let height = this.mount.clientHeight
 
-        return cube
-    }
+		const scene = new THREE.Scene()
 
-    start() {
-        if (!this.frameId) {
-            this.frameId = requestAnimationFrame(this.animate)
-        }
-    }
+		let camera = new THREE.PerspectiveCamera(
+			75,
+			width / height,
+			1,
+			10000000
+		)
+		const renderer = new THREE.WebGLRenderer({ antialias: true })
+		renderer.setClearColor('#000000')
+		renderer.setSize(width, height)
+		this.scene = scene
 
-    stop() {
-        cancelAnimationFrame(this.frameId)
-    }
+		scene.add(this.createCube())
 
-    animate() {
+		camera.position.z = 3
 
+		this.camera = camera
+		this.renderer = renderer
 
+		new OrbitControls(camera, renderer.domElement)
 
+		this.mount.appendChild(this.renderer.domElement)
+		this.start()
+	}
 
-        this.renderScene()
-        this.frameId = window.requestAnimationFrame(this.animate)
-    }
+	componentWillUnmount() {
+		this.stop()
+		this.mount.removeChild(this.renderer.domElement)
+	}
 
-    renderScene() {
+	createCube() {
+		const geometry = new THREE.BoxGeometry(1, 1, 1)
+		const material = new THREE.MeshBasicMaterial({ color: '#00ff00' })
+		const cube = new THREE.Mesh(geometry, material)
 
-        this.renderer.render(this.scene, this.camera)
-    }
+		return cube
+	}
 
-    render() {
-        return (
-            <div
-                style={{ overflow: 'hidden', maxHeight: '100vh', maxWidth: '100vw', width: '100vw', height: '100vh' }}
-                ref={(mount) => { this.mount = mount }}
-            />
-        )
-    }
+	start() {
+		if (!this.frameId) {
+			this.frameId = requestAnimationFrame(this.animate)
+		}
+	}
+
+	stop() {
+		cancelAnimationFrame(this.frameId)
+	}
+
+	animate() {
+		this.renderScene()
+		this.frameId = window.requestAnimationFrame(this.animate)
+	}
+
+	renderScene() {
+		this.renderer.render(this.scene, this.camera)
+	}
+
+	render() {
+		return (
+			<div
+				style={{
+					overflow: 'hidden',
+					maxHeight: '100vh',
+					maxWidth: '100vw',
+					width: '100vw',
+					height: '100vh'
+				}}
+				ref={mount => {
+					this.mount = mount
+				}}
+			/>
+		)
+	}
 }
